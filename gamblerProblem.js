696057eea04630c9eed95460bb1d18e2f3706990
shilpa <shilpa07udupi@gmail.com>
2020-11-18 22:23:12 +0530

    refactor: refactor to display won and lost days with stake value

--- gamblerProblem.js
+++ gamblerProblem.js
@@ -1,7 +1,10 @@
 var stakePerDay = 100;
 var betPerGame = 1;
+var wonDict = {};
+var lostDict = {};
 const WIN = 1;
 const MAX_DAY = 20;
+const INITIAL_STAKE = 100;
 /**
  * @method randomValue generates number 0 or 1
  */
@@ -39,17 +42,36 @@ var calculateStake = (callBack) => {
 
 /**
  * Calculate total amount won or lost after 20 days
+ * @var wonDict stores the days won as key and  stake as value
+ * @var lostDict store the days lost as key and stake as value
  */
-var stakeForMonth = () => {
+
+var stakeForMonth = (callBack) => {
     var totalAmount = 0;
     var currentDay = 1;
     while(currentDay <= MAX_DAY){
         calculateStake(makeBet);
         totalAmount += stakePerDay;
+        stakePerDay > INITIAL_STAKE ? wonDict[currentDay] = stakePerDay : lostDict[currentDay] = stakePerDay;
         currentDay ++;
         stakePerDay = 100;
     }
     console.log("Total amount won/lost : "+totalAmount);
+    callBack();
 }
 
-stakeForMonth();
+/**
+ * @description display won days and lost days with amount
+ */
+ var displayWonAndLostDays = () => {
+    console.log("\nDisplay won days and amount")
+    for(var key in wonDict) {
+        console.log(key + " : " + wonDict[key]);
+    }
+    console.log("\nDisplay lost days and amount")
+    for(var key in lostDict) {
+        console.log(key + " : " + lostDict[key]);
+    } 
+ }
+ 
+stakeForMonth(displayWonAndLostDays);
