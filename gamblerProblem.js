f0e784eabc2a7a818358884cef879231bc44fa53
shilpa <shilpa07udupi@gmail.com>
2020-11-18 19:46:59 +0530

    add: make bet so either win or loose

--- gamblerProblem.js
+++ gamblerProblem.js
@@ -1,2 +1,16 @@
 var stakePerDay = 100;
 var betPerGame = 1;
+const win = 1;
+
+/**
+ * @method randomValue generates number 0 or 1
+ */
+const randomValue = () => Math.floor(Math.random()*10)%2; 
+
+/**
+ * @description making bet and checking result
+ */
+var makeBet = () => {
+    var randomNumber = randomValue();
+    randomNumber == win ? console.log("Won") : console.log("Lost");
+}
